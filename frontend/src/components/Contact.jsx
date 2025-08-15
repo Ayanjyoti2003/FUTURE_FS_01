10e14e011ab4f37d347559819ebc7cb8efa3794d
Ayanjyoti2003 <ayanjyotibora8@gmail.com>
2025-08-15 13:51:50 +0530

    Updated API endpoint

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
         setStatus("Sending...");
 
         try {
-            const res = await fetch("http://localhost:5000/contact", { // Change if deployed
+            const res = await fetch("https://future-fs-01-xopq.onrender.com", { // Change if deployed
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
